163d089d9f350bd8d5e7d10a4eb4bf5c323d1c46
AmitRanganathan <Amit.ranganathan@gmail.com>
2019-02-13 18:15:12 -0800

    Added comments

--- homework1.php
+++ homework1.php
@@ -1,17 +1,29 @@
 
 <?php
+  /*
+    Main function which takes in the parameter passed in from the tesster_function
+    Does a loop from 1 to input, and checcks if each number is prime.  If it is,
+    number gets added to the array which will be printed at the end
+  */
   function prime_function($input){
     $primeNums = array();
     for($i = 1; $i <= $input; $i++){
       if(check_if_prime($i) == TRUE){
-        array_push($primeNums, $i);
+        array_push($primeNums, $i); //if number is prime, push to array
       }
     }
 
-    echo implode(", ", $primeNums);
-    echo("\n");
+    echo implode(", ", $primeNums); // Removes the last "," from the array, better for printing
+    echo("\n"); // line break
   }
 
+  /*
+    Helper method which checks if an input number is prime or not.
+    1 = not prime, 2 = prime, 3 = prime
+    If a number is even, it is not prime.
+    If input % any number uptil it == 0, then it is not prime.
+
+  */
   function check_if_prime($num){
     if($num == 1){
       return FALSE;
@@ -35,11 +47,14 @@
     return TRUE;
 
   }
-?>
+ ?>
 
 
 
 <?php
+  /*
+    Simple function which tests the functionality of the prime_function
+  */
   function tester_function(){
     echo("Output for parameter = 10: ");
     echo("\n");
@@ -64,5 +79,9 @@
     }*/
     //prime_function("12"); // Checking to see if it works when number is written as a string
   }
+ ?>
+
+<?php
+  //Call for tester function
   tester_function();
-?>
+ ?>
