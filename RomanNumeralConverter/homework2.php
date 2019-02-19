f7d48a66e8f5c1d0d57ba54ef6670515ca91572d
AmitRanganathan <Amit.ranganathan@gmail.com>
2019-02-19 14:30:37 -0800

    Renamed folder to contain no spaces

---
+++ RomanNumeralConverter/homework2.php
@@ -0,0 +1,19 @@
+<?php
+
+  /*
+    Let us use an array to store key-value pairs of roman numerals
+    I => 1, V =>5, etc..
+  */
+  $decoder = array(
+    'I'    => 1,
+    'V'    => 5,
+    'X'  => 10,
+    'L'    => 50,
+    'C'  => 100,
+    'D'    => 500,
+    "M"  => 1000,
+  );
+
+  echo $decoder[3];
+
+ ?>
