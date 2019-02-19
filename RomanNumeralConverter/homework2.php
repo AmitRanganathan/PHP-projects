4ed2c9468ed5617b59b5766fe0a1bd6a5dc80f3d
AmitRanganathan <Amit.ranganathan@gmail.com>
2019-02-19 15:17:10 -0800

    Created starter code and tester function.

--- RomanNumeralConverter/homework2.php
+++ RomanNumeralConverter/homework2.php
@@ -1,4 +1,15 @@
 <?php
+  /**
+  This program converts an input of roman numerals into the numerical system.  There
+  will be a validate_input function which first checks if the input is of correct type: String.
+  Then a convert_to_numeral function that will do the actual converting.  An array
+  will be used to map roman numerals into arabic numerals.  The map will be used to
+  do the conversion.
+
+  @author  Amit Ranganathan
+  @version 1.0
+  @since   2019-02-19
+  */
 
   /*
     Let us use an array to store key-value pairs of roman numerals
@@ -14,6 +25,40 @@
     "M"  => 1000,
   );
 
-  echo $decoder[3];
+  function validate_input($input){
+    if(!(is_string($input))){
+      throw new Exception("Input must be a string representing a Roman Numeral.");
 
+    }else{
+      echo($input);
+      echo('<br>');
+
+    }
+  }
+
+  function convert_to_numeral($romanString){
+    validate_input($romanString);
+  }
  ?>
+
+
+ <?php
+ /**
+  Tester function to test the functionality of the methods above.
+ */
+   function tester(){
+     try{
+       convert_to_numeral('V');
+       convert_to_numeral(3);
+       convert_to_numeral(4.56);
+     }catch(Exception $e){
+       echo 'Caught exception: ', $e->getMessage(), "\n";
+     }
+   }
+
+   // Call to tester function
+   tester();
+
+
+
+  ?>
